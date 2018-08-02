40e74b250bffd4de83c87965c1ac2cac127410ea
fjb14002 <filip.bicki@uconn.edu>
2018-08-02 18:59:33 -0400

    Added ability to find each event

--- RANGERtoPhyloXML.py
+++ RANGERtoPhyloXML.py
@@ -1,7 +1,11 @@
 import sys, xml.etree.cElementTree as ET
+from collections import defaultdict
 
 inputFile = sys.argv[1]
 
+#Finds where Reconcliation starts and ends
+#This is the section underneath the "Reconcilation:" and
+#right above the whitespace after the last event
 def findRec(lines) :
     start = 0
     end = 0
@@ -12,14 +16,28 @@ def findRec(lines) :
             end = lineNum
             return (start,end)
 
-def buildXML() :
+#This takes the locations of each event and creates the appropriate XML
+def buildXML(recLines) :
     root = ET.Element("recGeneTree")
     rooted = ET.SubElement(root, "phylogeny", rooted="true")
+    events = ("Transfer", "Duplication", "Speciation")
 
+    for line in recLines :
+        #if any(x in line for x in events) :
+        if events[0] in line :
+            #Transfer XML
+            print(events[0])
+        elif events[1] in line :
+                #Duplication XML
+                print(events[1])
+        elif events[2] in line :
+                #Speciation XML
+                print(events[2])
+        else :
+            print("Leaf")
     #hardcoded example
     clade = ET.SubElement(rooted, "clade")
     name = ET.SubElement(clade, "name").text = "m3"
-
 
     tree = ET.ElementTree(root)
     tree.write("file.xml")
@@ -29,6 +47,4 @@ with open(inputFile, 'r') as file :
     lines = file.readlines()
     s, e = findRec(lines)[0], findRec(lines)[1]
     recLines = lines[s:e]
-    buildXML()
-
-    
+    buildXML(recLines)
