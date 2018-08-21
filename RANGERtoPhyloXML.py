1f827111a9554c6a898a4db814819013248d67d5
fjb14002 <filip.bicki@uconn.edu>
2018-08-21 16:32:47 -0400

    Added XML creation for events and created proper output, finally working kind of

--- RANGERtoPhyloXML.py
+++ RANGERtoPhyloXML.py
@@ -1,4 +1,4 @@
-import sys, xml.etree.cElementTree as ET
+import sys
 import argparse
 from collections import defaultdict
 from Bio import Phylo
@@ -18,6 +18,7 @@ def findRec(lines) :
             end = lineNum
             return (start,end)
 
+#Takes the newick form of the trees from the file and returns them
 def findSpTree(lines) :
     for lineNum, current in enumerate(lines) :
         if(current.find("Species Tree:") != -1) :
@@ -30,47 +31,73 @@ def findGeneTree(lines) :
 
 #All XML generators found here
 #Extracts data from input lines and generates the appropriate XML
-def transferXML(line, root) :
+def transferXML(line,genetree) :
     
     #Data Extraction
     subNode = line[0:line.find(' =')]
     mapper = line[line.find('Mapping') + 12:line.find(', Recipient')]
     recip = line[line.find(', Recipient') + 16:len(line)]
     lca = line[line.find('[')+1:line.find(']:')].split(', ')
+    subString = "<name>" + subNode + "</name>"
+    for num, line in enumerate(genetree):
+        if line.find(subString) != -1:
+            leadingTabs = len(line) - len(line.lstrip())
+            tabs = '\t'*(int(leadingTabs/2)+1)
+            newLine = tabs + '<eventsRec>\n' + tabs + '\t<branchingOut speciesLocation=' + '\"' + mapper.rstrip() + '\"' + '></branchingOut>\n' + tabs + '</eventsRec>\n'
+            print(leadingTabs)
+            genetree.insert(num+1, newLine)
+        
+    return genetree
 
-    #Creating XML
-    #next = ET.SubElement(root, "clade")
-    #next = ET.SubElement(next, "transfer" + lca, mapper + " " + recip)
-    return(next)
-
-def duplicationXML(line, prev) :
+def duplicationXML(line,genetree) :
 
     #Data Extraction
     subNode = line[0:line.find(' =')]
     mapper = line[line.find('Mapping') + 12:len(line)]
     lca = line[line.find('[')+1:line.find(']:')].split(', ')
+    subString = "<name>" + subNode + "</name>"
+    for num, line in enumerate(genetree):
+        if line.find(subString) != -1:
+            leadingTabs = len(line) - len(line.lstrip())
+            tabs = '\t'*(int(leadingTabs/2)+1)
+            newLine = tabs + '<eventsRec>\n' + tabs + '\t<duplication speciesLocation=' + '\"' + mapper.rstrip() + '\"' + '></duplication>\n' + tabs + '</eventsRec>\n'
+            print(leadingTabs)
+            genetree.insert(num+1, newLine)
+        
+    return genetree
 
-    #Creating XML
-    next = prev
-    return(next)
-
-
-def speciationXML(line, prev) :
+def speciationXML(line, genetree) :
     
     #Data Extraction
     subNode = line[0:line.find(' =')]
     mapper = line[line.find('Mapping') + 12:len(line)]
     lca = line[line.find('[')+1:line.find(']:')].split(', ')
-    
-    #Creating XML
-    next = prev
-    return(next)
+    #insert into the genetree
+    subString = "<name>" + subNode + "</name>"
+    for num, line in enumerate(genetree):
+        if line.find(subString) != -1:
+            leadingTabs = len(line) - len(line.lstrip())
+            tabs = '\t'*(int(leadingTabs/2)+1)
+            newLine = tabs + '<eventsRec>\n' + tabs + '\t<speciation speciesLocation=' + '\"' + mapper.rstrip() + '\"' + '></speciation>\n' + tabs + '</eventsRec>\n'
+            print(leadingTabs)
+            genetree.insert(num+1, newLine)
+        
+    return genetree
 
-def leafXML(line, prev) :
-    subNode = line[0:line.find(': ')]
-    next = prev
-    return(next)
+def leafXML(line,genetree) :
+    leafName = line[0:line.find(': ')]
+    subString = "<name>" + leafName + "</name>"
+    for num, line in enumerate(genetree):
+        if line.find(subString) != -1:
+            leadingTabs = len(line) - len(line.lstrip())
+            tabs = '\t'*(int(leadingTabs/2)+1)
+            newLine = tabs + '<eventsRec>\n' + tabs + '\t<leaf speciesLocation=' + '\"' + leafName.rstrip() + '\"' + '></leaf>\n' + tabs + '</eventsRec>\n'
+            print(leadingTabs)
+            genetree.insert(num+1, newLine)
+        
+    return genetree
 
+#Creates XML tree using biopython based on inputted newick tree
 def buildTree(tree, qualifier) :
     with open("temp","w+") as temper :
         temper.writelines(tree)
@@ -84,7 +111,7 @@ def buildTree(tree, qualifier) :
     os.remove("temp")
     os.remove("temp2")
 
-    
+    #Inserting the recPhylo information to the gene/species tree
     for line in lines:
         line = "\t" + line
     if(qualifier == "s") :
@@ -96,41 +123,36 @@ def buildTree(tree, qualifier) :
     
     return lines
 
-
-
-
 #This takes the locations of each event and creates the appropriate XML
-def buildXML(recLines) :
-    root = ET.Element("recGeneTree")
-    rooted = ET.SubElement(root, "phylogeny", rooted="true")
+def buildXML(recLines,geneTree) :
     events = ("Transfer", "Duplication", "Speciation")
-    prev = root
     for line in recLines :
         if events[0] in line :
             #Transfer XML
-            next = transferXML(line,prev)
+            geneTree = transferXML(line,geneTree)
+
         elif events[1] in line :
             #Duplication XML
-            next = duplicationXML(line,prev)
+            geneTree = duplicationXML(line,geneTree)
+
         elif events[2] in line :
             #Speciation XML
-            next = speciationXML(line,prev)
+            geneTree = speciationXML(line,geneTree)
+
         else :
-            next = leafXML(line,prev)
-        prev = next
-    #hardcoded example
-    clade = ET.SubElement(rooted, "clade")
-    name = ET.SubElement(clade, "name").text = "m3"
+            geneTree = leafXML(line,geneTree)
 
-    tree = ET.ElementTree(root)
-    tree.write(outputFile)
 
-#Argument parser, checks input from command line       
+#Argument parser, checks input from command line, arguments are optional
 parser = argparse.ArgumentParser(description='Commands')
 parser.add_argument('-i', '--input', help="Input file path", default = sys.argv[1])
 parser.add_argument('-o', '--output', help="Output file name", default = 'Output')
 args = parser.parse_args()
-outputFile = args.output
+outputFile = args.outputFile
+
+#Hard coded input file for when biopython doesn't want to work
+#inputFile = r'C:\Win\CorePrograms\test'
+
 
 #Start of program, takes input and runs!
 with open(args.input,'r') as file :
@@ -142,9 +164,10 @@ with open(args.input,'r') as file :
     geneTreeNewick = findGeneTree(lines)
     spTree = buildTree(spTreeNewick, "s")
     geneTree = buildTree(geneTreeNewick, "g")
+    buildXML(recLines,geneTree)
 
-    with open("out", "w+") as outFile:
+
+    #If args.output doesn't work, change to any string file for the output
+    with open(args.output, "w+") as outFile:
         outFile.writelines(spTree)
         outFile.writelines(geneTree)
-
-    #buildXML(recLines)
